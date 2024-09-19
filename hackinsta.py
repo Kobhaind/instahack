6f2693b1be9ccee6f3b44f19cd1cac0a9c04bd55
Kobhaind <Obaidkh7852@gmail.com>
2024-09-19 11:31:24 +0530

    Update hackinsta.py
    .

--- hackinsta.py
+++ hackinsta.py
@@ -1,4 +1,12 @@
+# instahack
 
+pkg install git
+git clone and then paste this link
+pkg install python 
+cd instahack
+pip install request 
+pip install beautifulsoup4
+python hackinsta.py 
 '''
 TODO LIST:
 	Fix and make proxy function better
